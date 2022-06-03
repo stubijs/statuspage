21926660eec549b50070174b66027f3ea32c61ff
stubijs <34370917+stubijs@users.noreply.github.com>
2022-06-03 21:30:11 +0200

    test

--- packages/status-page/docs/.vitepress/config.ts
+++ packages/status-page/docs/.vitepress/config.ts
@@ -14,9 +14,8 @@ export default defineConfig({
   head: [
     ['meta', { name: 'theme-color', content: '#ffffff' }],
     // Only 4 Icons
-    ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml', sizes: 'any' }],
+    ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg', sizes: 'any' }],
     ['link', { rel: 'icon', href: '/favicon.png', type: 'image/png', sizes: '32x32' }],
-    ['link', { rel: 'icon', href: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' }],
     ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
     ['link', { rel: 'manifest', href: '/site.webmanifest' }],
     ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ffc40d' }],
