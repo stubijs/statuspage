ef4117fcaba9de71fc81a0fcd1763ed34e1f7d0f
stubijs <34370917+stubijs@users.noreply.github.com>
2022-06-18 22:35:01 +0200

    fix: lint

--- packages/locations/main.ts
+++ packages/locations/main.ts
@@ -1,54 +1,58 @@
-import fetch from 'node-fetch';
-import pkg from 'fs-extra';
-const { writeJson } = pkg;
+import fetch from 'node-fetch'
+import pkg from 'fs-extra'
+const { writeJson } = pkg
 
 interface dataCFResponse {
-    iata: string;
-    lat: number;
-    lon: number;
-    cca2: string;
-    region: string;
-    city: string;
+  iata: string
+  lat: number
+  lon: number
+  cca2: string
+  region: string
+  city: string
 }
 
-interface locData { 
-    lat: number;
-    lon: number;
-    cca2: string;
-    region: string;
-    city: string; 
-} 
+interface locData {
+  lat: number
+  lon: number
+  cca2: string
+  region: string
+  city: string
+}
 
 interface locDataFin {
-    [key: string]: locData;
- }
+  [key: string]: locData
+}
 
 // Fetch the cloudflare locations
 const init = {
-    method: 'GET',
-    cache: <RequestCache>'no-store',
+  method: 'GET',
+  cache: <RequestCache>'no-store',
 }
 
 const monitorUrl = 'https://speed.cloudflare.com/locations'
 
-const response = await fetch(monitorUrl, init);
-const data = await response.json() as Array<dataCFResponse>;
+const response = await fetch(monitorUrl, init)
+const data = await response.json() as Array<dataCFResponse>
 
 const finData: locDataFin = {}
 
-data.forEach(obj => {
-    finData[obj.iata] = {
-        'lat': obj.lat,
-        'lon': obj.lon,
-        'cca2': obj.cca2,
-        'region': obj.region,
-        'city': obj.city,
-    }
-});
+data.forEach((obj) => {
+  finData[obj.iata] = {
+    lat: obj.lat,
+    lon: obj.lon,
+    cca2: obj.cca2,
+    region: obj.region,
+    city: obj.city,
+  }
+})
 
 writeJson('./locations.json', finData, (err: any) => {
-    if (err) return console.error(err)
-    console.log('success!')
-  })
+  if (err)
 
-export{}
+    return console.error(err)
+
+  // eslint-disable-next-line no-console
+  console.log('success!')
+})
+
+export {}
